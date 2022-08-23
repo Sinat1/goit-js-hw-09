65aa39a0291b34860efb70577a07c6abdc6e1386
Sinat1 <67416969+Sinat1@users.noreply.github.com>
2022-08-23 20:06:10 +0000

    Deploying to gh-pages from @ Sinat1/goit-js-hw-09@9ba5f138c58293cc1ce80aa316d69417b91308b9 🚀

--- 01-color-switcher.5f5cf075.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.stopBtn.disabled=!0;t.startBtn.addEventListener("click",(()=>{t.startBtn.disabled=!0,t.stopBtn.disabled=!1,timerId=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)}));t.stopBtn.addEventListener("click",(()=>{t.stopBtn.disabled=!0,t.startBtn.disabled=!1,clearInterval(timerId)}));
-//# sourceMappingURL=01-color-switcher.5f5cf075.js.map
